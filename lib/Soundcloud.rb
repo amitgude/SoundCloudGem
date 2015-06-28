a09f63f4383598e8108ad3594feb1e233adf67f7
Amit <gudeamit148@gmail.com>
2015-06-28 14:42:35 +0530

    added retrieving of playlist of a user

--- lib/Soundcloud.rb
+++ lib/Soundcloud.rb
@@ -10,7 +10,10 @@ module Soundcloud
    tracks  = get("http://api.soundcloud.com/users/#{user_id}/tracks.json?client_id=b996850b65616c954995baf1721b3ac4")
   end
 
-
+  def self.playlist(username,playlist_name)
+   playlist_id = find_playlist_id(username,playlist_name)
+   get("http://api.soundcloud.com/playlists/#{playlist_id}.json?client_id=b996850b65616c954995baf1721b3ac4")
+  end
 
 end
 
@@ -18,4 +21,8 @@ private
 
   def find_user_id(username)
   	 get("http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/#{username}&client_id=b996850b65616c954995baf1721b3ac4").parsed_response["id"]
+  end
+
+  def find_playlist_id(username, playlist)
+   get("http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/#{username}/sets/#{playlist}&client_id=b996850b65616c954995baf1721b3ac4").parsed_response["id"]
   end
